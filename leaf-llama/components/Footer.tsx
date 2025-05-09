3c0379810af5d246cb5fd3c814ba695d58db9233
KingDonutOfAllDonuts <sirdonut12345@gmail.com>
2025-05-09 10:43:21 -0500

    test

--- leaf-llama/components/Footer.tsx
+++ leaf-llama/components/Footer.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { navButtons } from "@/lib/constants";
 import { ArrowUp, Mail, MapPinned, Phone } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import icon from "@/app/icon.png";
+import { FaFacebook, FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="bg-green-900 text-white py-6 px-5 overflow-x-hidden w-full">
+    <div className="bg-green-900 flex items-center flex-col text-white py-6 px-5 overflow-x-hidden w-full">
       <div className="w-full flex justify-center absolute -translate-y-full left-0">
         <ArrowUp
           className="text-white w-[50px] h-[50px] rounded-full bg-green-900 cursor-pointer p-3 transition-all hover:-translate-y-0.5"
@@ -17,48 +19,45 @@ const Footer = () => {
           }}
         />
       </div>
-      <div className="w-full flex flex-col justify-center items-center">
-        <div className="max-w-3xl border-b px-2 py-2 flex flex-row justify-between w-full">
-          <div className="flex flex-col">
-            <h2 className="text-3xl font-kaushan">Leaf Llama</h2>
-            <p className="text-xs">Lettuce eat healthy and fresh!</p>
-          </div>
-          {/* <a
-            href="https://github.com/KingDonutOfAllDonuts/Leaf-Llama"
-            className="flex items-center shadow-[0_0_0_2px_#000000_inset] px-4 space-x-2 text-white rounded-lg transform hover:-translate-y-0.5 transition duration-400"
-          >
-            <FaGithub size={20} />
-            <span className="text-sm">Github repo</span>
-          </a> */}
+      <div className="w-full max-w-6xl px-5 flex flex-row items-center">
+        <Image
+          src={icon}
+          alt=""
+          className={`w-12 h-12 object-contain brightness-0 invert-[1]`}
+        />
+        <div className="flex flex-col">
+          <h2 className="text-4xl font-kaushan">Leaf Llama</h2>
+          {/* <p className="text-xs">Lettuce eat healthy and fresh!</p> */}
         </div>
+      </div>
 
-        <div className="max-w-3xl px-2 py-2 flex flex-row justify-between w-full">
-          <div className="flex flex-col space-y-3 flex-1 px-3">
-            <h1 className="text-base md:text-xl font-[450] w-full border-b-2">
-              Leaf Llama
-            </h1>
+      <div className="w-full flex flex-row max-w-6xl px-5 justify-between items-center pb-5 border-b">
+        <div className="flex flex-row justify-between w-full">
+          <div className="flex flex-col space-y-3 flex-1">
+            <p className="text-sm mt-2">Lettuce eat healthy and fresh!</p>
             <div className="flex flex-row space-x-2">
               <Mail />
-              <p className="text-sm md:text-base font-[300] font-lora">
+              <p className="text-sm md:text-base font-[300]">
                 info@leaf-llama.com
               </p>
             </div>
             <div className="flex flex-row space-x-2">
               <Phone />
-              <p className="text-sm md:text-base font-[300] font-lora">
-                (555)-555-5555
-              </p>
+              <p className="text-sm md:text-base font-[300]">(555)-555-5555</p>
             </div>
             <div className="flex flex-row space-x-2">
               <MapPinned />
-              <p className="text-sm md:text-base font-[300] font-lora">
-                Address
-              </p>
+              <p className="text-sm md:text-base font-[300]">Address</p>
+            </div>
+            <div className="flex flex-row w-full py-3 gap-5">
+              <FaInstagram className="w-7 h-7 hover:text-gray-200 cursor-pointer transition-all" />
+              <FaFacebook className="w-7 h-7 hover:text-gray-200 cursor-pointer transition-all" />
+              <FaYoutube className="w-7 h-7 hover:text-gray-200 cursor-pointer transition-all" />
             </div>
           </div>
 
-          <div className="flex flex-col space-y-3 flex-1 px-3">
-            <h1 className="text-base md:text-xl font-[450] w-full border-b-2">
+          <div className="flex flex-col space-y-3 flex-1">
+            <h1 className="text-base md:text-xl font-[450] w-full border-b">
               Pages
             </h1>
             {Object.keys(navButtons).map((v, i) => {
@@ -70,7 +69,7 @@ const Footer = () => {
                 <Link
                   href={"/" + route}
                   key={i}
-                  className="text-sm md:text-base font-[300] font-lora cursor-pointer hover:underline"
+                  className="text-sm md:text-base font-[300] cursor-pointer hover:underline"
                 >
                   {v.replaceAll("_", " ")}
                 </Link>
@@ -79,7 +78,7 @@ const Footer = () => {
           </div>
 
           <div className="flex flex-col space-y-3 flex-1 px-3">
-            <h1 className="text-xl font-[450] w-full border-b-2 pt-[24px] md:pt-[28px]"></h1>
+            <h1 className="text-xl font-[450] w-full border-b pt-[24px] md:pt-[28px]"></h1>
             {Object.keys(navButtons).map((v, i) => {
               if (i < 3) {
                 return "";
@@ -92,7 +91,7 @@ const Footer = () => {
                 <Link
                   href={"/" + route}
                   key={i}
-                  className="text-sm md:text-base font-[300] font-lora cursor-pointer hover:underline"
+                  className="text-sm md:text-base font-[300] cursor-pointer hover:underline"
                 >
                   {v.replaceAll("_", " ")}
                 </Link>
@@ -100,11 +99,12 @@ const Footer = () => {
             })}
           </div>
         </div>
-        <p className="text-sm pt-3">
-          &copy; {new Date().getFullYear()} Leaf Llama. All rights reserved.
-          (This is a ficticious website.)
-        </p>
       </div>
+
+      <p className="text-sm pt-3">
+        &copy; {new Date().getFullYear()} Leaf Llama. All rights reserved. (This
+        is a ficticious website.)
+      </p>
     </div>
   );
 };
